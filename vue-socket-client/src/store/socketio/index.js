ba1e7b73b5c3c84cd29b971e5e7c5f1c770e11cd
Dylanson25 <dvillarreal0@ucol.mx>
2022-09-18 22:54:34 -0500

    add/getters and actions in vuex

--- vue-socket-client/src/store/socketio/index.js
+++ vue-socket-client/src/store/socketio/index.js
@@ -1,13 +1,13 @@
 import state from "./state";
 import * as mutations from "./mutations";
+import * as getters from "./getters";
+import * as actions from "./actions";
 const socketio = {
   namespaced: true,
   state,
   mutations,
-  //   getters: {},
-  //   actions: {
-  //     //emitimos acciones desde nuestra app al servidor de nodejs con socket.io
-  //   },
+  getters,
+  actions,
 };
 
 export default socketio;
