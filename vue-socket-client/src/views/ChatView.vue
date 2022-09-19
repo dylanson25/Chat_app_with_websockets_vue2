4236d0fd5b33dba685bde577572988b1a7ffa353
Dylanson25 <dvillarreal0@ucol.mx>
2022-09-18 21:08:50 -0500

    create/chatview whit sidebar and chatrroom implemented

---
+++ vue-socket-client/src/views/ChatView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="cont">
+        <Sidebar />
+        <router-view />
+    </div>
+</template>
+
+<script>
+import { Sidebar, ChatRoom } from "@/layouts"
+export default {
+    components: {
+        Sidebar,
+        ChatRoom,
+    }
+}
+</script>
+
+<style lang="sass" scoped>
+.cont 
+  align-items: center
+  background-image: url('../assets/fondo_login.png')
+  display: flex
+  height: 100%
+  justify-content: center
+  margin: 0
+  width: 100%
+</style>
