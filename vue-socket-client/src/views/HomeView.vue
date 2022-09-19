cce6e5b6811fd388b2c0add7ab5f70ecde61cd23
Dylanson25 <dvillarreal0@ucol.mx>
2022-09-18 21:07:38 -0500

    add / logint to homeview

--- vue-socket-client/src/views/HomeView.vue
+++ vue-socket-client/src/views/HomeView.vue
@@ -1,6 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+  <div class="cont">
+    <div class="card">
+      <div class="card-content">
+        <div class="title">
+          Escribe tu nombre...
+        </div>
+        <hr>
+        <div class="content">
+          <b-field>
+            <b-input v-model="userName" size="is-medium" icon="account" placeholder="Nombre de usuario" />
+          </b-field>
+        </div>
+        <hr>
+        <footer class="card-footer">
+          <b-button type="is-primary is-light" size="is-medium" class="card-footer-item" @click="()=> handleNavigate()">
+            Entrar</b-button>
+        </footer>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,5 +27,37 @@
 
 export default {
   name: 'HomeView',
+  methods: {
+    handleNavigate() {
+      this.$router.push('/chats/global-chat')
+    },
+  },
+  data() {
+    return {
+      userName: ""
+    }
+  }
 }
 </script>
+
+<style lang="css" scoped>
+.cont {
+  background-image: url('../assets/fondo_login.png');
+  display: flex;
+  margin: 0;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
+
+
+.card {
+  width: 400px;
+  margin: 16px;
+}
+
+.title {
+  font-size: 1.5rem;
+}
+</style>
