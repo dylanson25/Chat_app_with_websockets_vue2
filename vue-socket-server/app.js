d0730ea6b1d6b0a6b11cf910b46a574f751e4dae
Dylanson25 <dvillarreal0@ucol.mx>
2022-09-24 11:07:05 -0500

    new model to WS

--- vue-socket-server/app.js
+++ vue-socket-server/app.js
@@ -11,51 +11,136 @@ const { Server } = require("socket.io", {
 });
 const io = new Server(server);
 
-let users = {};
-let listUsers = [];
-
 app.get("/", (req, res) => {
   res.send("Socketio...");
 });
 
+let users = [];
+let userList = [];
+let chatRooms = [];
+/**
+ * Arrego de objetos con la informacion del usario se crea cuando se hace login
+ * users{
+ *    {
+    uid: 1,
+    userName: "Dylan",
+    chats: [{ id: "wj3UfZTmCO_tHTodAAAF", name: "Chat Globlal" }],
+  },
+  {
+    uid: 2,
+    userName: "Ximena",
+    chats: [{ id: "wj3UfZTmCO_tHTodAAAF", name: "Chat Globlal" }],
+  },
+  {
+    uid: 3,
+    userName: "Sierra",
+    chats: [{ id: "wj3UfZTmCO_tHTodAAAF", name: "Chat Globlal" }],
+  },
+ * }
+ * -------------------------------------
+ * listado de usarios conectados
+ * usersList ["Dylan", "Ximena", "Sierra"]
+ * -------------------------------------
+ * Arrglode objetos con los chats entre usarios
+ * chats{
+ *    id
+ *     messages [
+ *        {
+ *          userName,
+ *          msg,
+ *        }
+ *        {
+ *          userName,
+ *          msg,
+ *        }
+ * ]
+ * }
+ *
+ */
+
+const userUIDFilter = (uid) => {
+  const user = users.filter((user) => user.uid === uid);
+  return user.length != 0 ? user : false;
+};
+
+const chatRoomsUIDFilter = (id) => {
+  const chatRoom = chatRooms.filter((chatRoom) => chatRoom.id === id);
+  return chatRoom.length != 0 ? chatRoom : false;
+};
+
 io.on("connection", (socket) => {
   console.log("a user connected");
-  /* eventos chat */
-  // EVENT LOGIN
-  socket.on("login", (username) => {
-    console.log("LOGIN: ", username);
 
-    if (users[username]) {
+  // EVENT LOGIN
+  socket.on("login", ({ uid, userName }) => {
+    console.log("LOGIN: " + userName);
+
+    if (userUIDFilter(uid)) {
       socket.emit("USER_EXIST");
       return;
     }
-    socket.username = username;
-    users[username] = username;
-    listUsers.push(username);
+
+    socket.user = { uid, userName };
+    users.push({ uid, userName, chats: [] });
+    userList.push({ uid, userName });
 
     socket.emit("LOGIN", {
-      username: socket.username,
-      listUsers,
+      userName: userName,
+      userList,
+      chatRooms,
     });
+
     socket.broadcast.emit("JOINED", {
-      username: socket.username,
-      listUsers,
+      userName: userName,
+      userList,
+      users,
     });
   });
   //EVENT NEWMESSAGE
-  socket.on("newMessage", (message) => {
+  socket.on("newMessage", ({ id, message, toUserUid }) => {
     console.log("NEW MESSAGE");
-    socket.broadcast.emit("NEW MESSAGE", `${socket.username}: message`);
-    socket.emit("NEW_MESSAGE", `Yo: ${message}`);
+
+    let chatRoom = chatRoomsUIDFilter(id);
+
+    if (!chatRoom) {
+      //Si no existe se crea
+      const idx = users.findIndex(({ uid }) => toUserUid === uid);
+      users[idx].chats.push("wj3UfZTmCO_tHTodAAAF");
+
+      chatRooms.push({
+        id,
+        messages: [{ userName: socket.user.name, msg: message }],
+      });
+
+      socket.emit("UPDATE_USERS", users);
+    } else {
+      chatRoom.messages.push({ userName: socket.user.name, msg: message });
+
+      const idx = chatRooms.findIndex(({ id }) => chatRoom.id === id);
+      chatRooms[idx] = chatRoom;
+    }
+    socket.broadcast.emit("NEW_MESSAGE", `${socket.user.name}: ${message}`);
+    socket.emit("LOAD_CHAT", chatRooms);
+
+    // socket.emit("NEW_MESSAGE", `Yo: ${message}`);
   });
   //EVENT DISCONET
   socket.on("disconnect", () => {
-    console.log("desconectado: ", socket.username);
-    if (users[socket.username]) {
-      delete users[socket.username];
+    console.log("DISCONNECTED: ", socket.user?.userName);
+
+    let user = userUIDFilter(socket.user);
+
+    if (user) {
+      const idx = users.findIndex(({ uid }) => user.uid === uid);
+      users.splice(idx, 1);
+
+      const uidx = userList.findIndex(({ uid }) => user.uid === uid);
+      userList.splice(uidx, 1);
+
       socket.broadcast.emit("USER_LEFT", {
-        username: socket.username,
+        userName: socket.user.userName,
         users,
+        userList,
       });
     }
   });
